c6a4a37c79530d4f1ef95b5bee25887ebcdf48eb
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-20 20:49:34 +0600

    Symbol table

--- src/main.py
+++ src/main.py
@@ -1,20 +1,59 @@
 from lexer import TokenScanner
+from parser import SyntaxProcessor
 
 code = '''
 int main() {
     int x = 10;
-    return x;
+    int y = 20;
+    int z = x + y;
+    print(z);
+    return 0;
 }
 '''
 
+# Test Lexer
+print("=" * 50)
+print("LEXICAL ANALYSIS")
+print("=" * 50)
 scanner = TokenScanner()
-tokens, issues = scanner.scan(code)
+tokens, lex_issues = scanner.scan(code)
 
-print("=== TOKENS ===")
+print("\n=== TOKENS ===")
 for tok in tokens:
     print(f"Token(kind='{tok['kind']}', value={tok['val']!r}, line={tok['ln']}, col={tok['col']})")
 
-if issues:
-    print("\n=== ISSUES ===")
-    for issue in issues:
+if lex_issues:
+    print("\n=== LEXER ISSUES ===")
+    for issue in lex_issues:
+        print(issue)
+
+# Test Parser
+print("\n" + "=" * 50)
+print("SYNTAX ANALYSIS")
+print("=" * 50)
+
+# Create token input string for parser
+scanner2 = TokenScanner()
+scanner2.scan(code)
+parser = SyntaxProcessor()
+parser.initialize()
+
+# Parse using the lexer
+lexer = scanner2.scanner
+result = parser.processor.parse(code, lexer=lexer)
+
+print("\n=== AST ===")
+for node in parser.ast:
+    print(node)
+
+print("\n=== INTERMEDIATE REPRESENTATION ===")
+for i, instr in enumerate(parser.ir_instructions):
+    print(f"{i}: {instr}")
+
+print("\n=== SYMBOL TABLE ===")
+parser.registry.show()
+
+if parser.issues:
+    print("\n=== PARSER ISSUES ===")
+    for issue in parser.issues:
         print(issue)
---
+++ src/symbol_table.py
@@ -0,0 +1,72 @@
+class VariableRegistry:
+    def __init__(self):
+        self.scope_stack = [{}]
+        self.scope_names = ['global']
+        self.current_scope_id = 0
+
+    def add(self, identifier, var_type, initial_val=None, context='declaration'):
+        current_scope = self.scope_stack[-1]
+        current_scope[identifier] = {
+            'id': identifier,
+            'dtype': var_type,
+            'val': initial_val,
+            'ctx': context,
+            'scope': self.scope_names[-1],
+            'scope_level': len(self.scope_stack) - 1
+        }
+
+    def find(self, identifier):
+        for scope in reversed(self.scope_stack):
+            if identifier in scope:
+                return scope[identifier]
+        return None
+
+    def find_in_current_scope(self, identifier):
+        current_scope = self.scope_stack[-1]
+        return current_scope.get(identifier)
+
+    def update(self, identifier, new_value):
+        for scope in reversed(self.scope_stack):
+            if identifier in scope:
+                scope[identifier]['val'] = new_value
+                return True
+        return False
+
+    def all_entries(self):
+        all_vars = []
+        for scope in self.scope_stack:
+            for var_info in scope.values():
+                all_vars.append(var_info)
+        return all_vars
+
+    def current_scope_entries(self):
+        current_scope = self.scope_stack[-1]
+        return list(current_scope.values())
+
+    def push_scope(self, scope_name=None):
+        if scope_name is None:
+            self.current_scope_id += 1
+            scope_name = f"scope_{self.current_scope_id}"
+        self.scope_stack.append({})
+        self.scope_names.append(scope_name)
+
+    def pop_scope(self):
+        if len(self.scope_stack) > 1:
+            popped_scope = self.scope_stack.pop()
+            self.scope_names.pop()
+            return popped_scope
+        return None
+
+    def get_scope_level(self):
+        return len(self.scope_stack) - 1
+
+    def get_current_scope_name(self):
+        return self.scope_names[-1]
+
+    def is_declared_in_current_scope(self, identifier):
+        return identifier in self.scope_stack[-1]
+
+    def clear(self):
+        self.scope_stack = [{}]
+        self.scope_names = ['global']
+        self.current_scope_id = 0
